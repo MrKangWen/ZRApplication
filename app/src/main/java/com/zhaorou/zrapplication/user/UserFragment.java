c90036a1860da226a72dddb3dd9b88d065a5af77
lykungit <332329853@qq.com>
2018-07-10 20:56:42 +0800

    接口接入,viewpger只有需要无限滑动的才需使用FragmentStatePagerAdapter

--- app/src/main/java/com/zhaorou/zrapplication/user/UserFragment.java
+++ app/src/main/java/com/zhaorou/zrapplication/user/UserFragment.java
@@ -6,25 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.zhaorou.zrapplication.R;
+import com.zhaorou.zrapplication.base.GlideApp;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
 
+    @BindView(R.id.fragment_user_avatar_iv)
+    ImageView mAvatarIv;
+
+    private View mView;
+    private Unbinder mUnbinder;
 
     public UserFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.fragment_user, container, false);
+            mUnbinder = ButterKnife.bind(this, mView);
+        }
+        GlideApp.with(this).load(R.mipmap.ic_launcher).circleCrop().into(mAvatarIv);
+        return mView;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
+    }
 }
