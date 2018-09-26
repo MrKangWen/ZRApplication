c28b15d70dcc911aabf20a0fe7fe1466a184e58b
zhangkangbin <zhangkangbin>
2018-09-26 22:09:40 +0800

    base

---
+++ app/src/main/java/com/zhaorou/zrapplication/base/BaseDataModel.java
@@ -0,0 +1,17 @@
+package com.zhaorou.zrapplication.base;
+
+/**
+ * base data
+ */
+public class BaseDataModel extends BaseModel{
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private String data;
+
+}
